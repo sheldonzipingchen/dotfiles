49a4610ebe9981f09cf070fd42968e432e5f7cc3
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2023-08-29 07:39:51 +0800

    update setup.sh files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-[ ! -d "$HOME/.config/wezterm" ] && ln -s ~/dotfiles/wezterm ~/.config/wezterm
-[ ! -d "$HOME/.config/nvim" ] && ln -s ~/dotfiles/nvim ~/.config/nvim
+echo "Setting wezterm configuration files."
+
+if [ -d "$HOME/.config/wezterm" ]; then
+	echo "wezterm configuration files already has been setted. "
+else
+	ln -s ~/dotfiles/wezterm ~/.config/wezterm
+fi
+
+echo "Wezterm configuration files is finished."
+
+echo "Setting neovim configuration files."
+
+if [ -d "$HOME/.config/nvim" ]; then
+	echo "Neovim configuration files already has been setted."
+else
+	ln -s ~/dotfiles/nvim ~/.config/nvim
+fi
+
+echo "Neovim configuration files is finished."
