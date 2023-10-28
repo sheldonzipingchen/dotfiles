7216eac5cf5fadcbbe6c33bfb75e89847a8fa068
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2023-10-28 11:05:05 +0800

    install neotree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,7 +18,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 return require('lazy').setup({
-	"folke/which-key.nvim",
+    {
+        "folke/which-key.nvim",
+        lazy = true,
+    },
+
+    -- neo-truee
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        keys = {
+            {
+                "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree",
+            }
+        },
+        config = function()
+            require("neo-tree").setup()
+        end,
+    },
 
 	-- Themes
 	{ 
