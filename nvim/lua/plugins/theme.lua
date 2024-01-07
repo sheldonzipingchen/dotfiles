70f2cd9911406f53faaad4f8eb7b95434b8a8ed5
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2024-01-07 21:10:37 +0800

    更新 nvim 配置

--- nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return { 
-    "marko-cerovac/material.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.material_style = 'palenight'
-        vim.cmd [[colorscheme material]]
-    end,
-}
