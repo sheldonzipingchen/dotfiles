8906186db0a48547c298a27ef081adc96851159c
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2024-02-02 07:33:38 +0800

    oh my fish 配置文件

---
+++ my-fish/custom_env.fish
@@ -0,0 +1,6 @@
+alias vim nvim
+set -gx PATH "$PATH:/Users/sheldon/go/bin"
+set -gx EDITOR nvim
+set -gx https_proxy "http://127.0.0.1:7890"
+set -gx http_proxy "http://127.0.0.1:7890"
+set -gx all_proxy "http://127.0.0.1:7890"
