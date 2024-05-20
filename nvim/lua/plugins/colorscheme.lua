ea9731d7b3556afbca799dd797b1d028904c4ff4
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2024-05-20 23:31:17 +0800

    更新 color scheme 配置

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,11 +3,11 @@ return {
         "folke/tokyonight.nvim",
         lazy = true,
         opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
+            -- transparent = true,
+            -- styles = {
+            -- sidebars = "transparent",
+            -- floats = "transparent",
+            -- },
         },
     },
     {
